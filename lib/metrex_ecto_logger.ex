8e56a4c33fbeced50e5b728fb533ac69afbe98b3
Brad Condo <bradcondo@gmail.com>
2016-11-07 12:49:07 -0500

    Added debug statements

--- lib/metrex_ecto_logger.ex
+++ lib/metrex_ecto_logger.ex
@@ -5,6 +5,7 @@ defmodule MetrexEctoLogger do
     (log_entry.queue_time || 0) / 1_000 |> ExStatsD.histogram("ecto.queue.time")
     # @TODO - increment a counter for query type? (SELECT, INSERT, UDPATE, etc...)
     # @TODO - incrememt a counter for primary table?
+    IO.inspect("query")
     ExStatsD.increment("ecto.query.count")
   end
   
