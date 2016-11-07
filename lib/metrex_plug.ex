3a67716c97a0c8b01078fbe291a5b2a70ae226fa
Brad Condo <bradcondo@gmail.com>
2016-11-07 13:57:01 -0500

    Updates to path on connection/plug logging

--- lib/metrex_plug.ex
+++ lib/metrex_plug.ex
@@ -6,7 +6,8 @@ defmodule MetrexPlug do
 
   def call(conn, _config) do
     ExStatsD.increment("req.count")
-    IO.inspect(conn)
+    path = Enum.reject(conn.path_info, fn(x) -> Regex.match?(~r/^[0-9]+$/, x) end) |> Enum.join(".")
+    ExStatsD.increment("req.#{conn.method}.#{path}")
     before_time = :os.timestamp
     register_before_send conn, fn conn ->
       :timer.now_diff(:os.timestamp, before_time) / 1_000 |> ExStatsD.histogram("resp.time")
