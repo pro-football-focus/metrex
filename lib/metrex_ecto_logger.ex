51fe3142771fbf843249803a322896d171dc1b5f
Brad Condo <bradcondo@gmail.com>
2016-11-07 13:34:28 -0500

    Added additional tracking metrics

--- lib/metrex_ecto_logger.ex
+++ lib/metrex_ecto_logger.ex
@@ -1,11 +1,10 @@
 defmodule MetrexEctoLogger do
 
   def log(log_entry) do
-    IO.inspect(log_entry)
-    log_entry.query_time / 1_000_000 |> ExStatsD.histogram("ecto.query.time")
-    (log_entry.queue_time || 0) / 1_000_000 |> ExStatsD.histogram("ecto.queue.time")
-    # @TODO - increment a counter for query type? (SELECT, INSERT, UDPATE, etc...)
-    # @TODO - incrememt a counter for primary table?
+    System.convert_time_unit(log_entry.decode_time, :native, :milliseconds) |> ExStatsD.histogram("ecto.decode.time")
+    System.convert_time_unit(log_entry.query_time, :native, :milliseconds) |> ExStatsD.histogram("ecto.query.time")
+    System.convert_time_unit(log_entry.queue_time, :native, :milliseconds) |> ExStatsD.histogram("ecto.queue.time")
+    ExStatsD.increment("ecto.table.#{log_entry.source}")
     ExStatsD.increment("ecto.query.count")
   end
   
--- lib/metrex_plug.ex
+++ lib/metrex_plug.ex
@@ -5,12 +5,13 @@ defmodule MetrexPlug do
   def init(opts), do: opts
 
   def call(conn, _config) do
+    ExStatsD.increment("req.count")
+    method = String.downcase(conn.method)
+    path = Enum.join(conn.path_info, ".")
+    ExStatsD.increment("req.#{method}.#{path}")
     before_time = :os.timestamp
-
     register_before_send conn, fn conn ->
       :timer.now_diff(:os.timestamp, before_time) / 1_000 |> ExStatsD.histogram("resp.time")
-      IO.inspect(conn)
-      ExStatsD.increment("resp.count")
       conn
     end
   end
