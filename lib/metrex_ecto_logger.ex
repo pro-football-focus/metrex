10ce72f157eb90558dac6aa6d83a9ef31205b763
Brad Condo <bradcondo@gmail.com>
2016-11-07 13:20:23 -0500

    Testing for 0.1.3

--- lib/metrex_ecto_logger.ex
+++ lib/metrex_ecto_logger.ex
@@ -1,11 +1,11 @@
 defmodule MetrexEctoLogger do
 
   def log(log_entry) do
-    log_entry.query_time / 1000 |> ExStatsD.histogram("ecto.query.time")
-    (log_entry.queue_time || 0) / 1_000 |> ExStatsD.histogram("ecto.queue.time")
+    IO.inspect(log_entry)
+    log_entry.query_time / 1_000_000 |> ExStatsD.histogram("ecto.query.time")
+    (log_entry.queue_time || 0) / 1_000_000 |> ExStatsD.histogram("ecto.queue.time")
     # @TODO - increment a counter for query type? (SELECT, INSERT, UDPATE, etc...)
     # @TODO - incrememt a counter for primary table?
-    IO.inspect("query")
     ExStatsD.increment("ecto.query.count")
   end
   
--- lib/metrex_plug.ex
+++ lib/metrex_plug.ex
@@ -5,14 +5,12 @@ defmodule MetrexPlug do
   def init(opts), do: opts
 
   def call(conn, _config) do
-    IO.inspect("call - started")
     before_time = :os.timestamp
 
     register_before_send conn, fn conn ->
       :timer.now_diff(:os.timestamp, before_time) / 1_000 |> ExStatsD.histogram("resp.time")
-      # @TODO - increment a counter for the URI
+      IO.inspect(conn)
       ExStatsD.increment("resp.count")
-      IO.inspect("register_before_send - complete")
       conn
     end
   end
